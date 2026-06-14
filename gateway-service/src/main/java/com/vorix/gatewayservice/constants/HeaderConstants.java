9ebe238651ced29ec567eb92918a084d534089cb
OM-JADHAV25 <omjadhav2562006@gmail.com>
2026-06-14 20:24:04 +0530

    feat(gateway): implement JWT validation and user context propagation

---
+++ gateway-service/src/main/java/com/vorix/gatewayservice/constants/HeaderConstants.java
@@ -0,0 +1,13 @@
+package com.vorix.gatewayservice.constants;
+
+public final class HeaderConstants {
+
+    private HeaderConstants() {
+    }
+
+    public static final String USER_ID = "X-User-Id";
+
+    public static final String USER_EMAIL = "X-User-Email";
+
+    public static final String USER_ROLES = "X-User-Roles";
+}
--- gateway-service/src/main/java/com/vorix/gatewayservice/filter/JwtAuthenticationFilter.java
+++ gateway-service/src/main/java/com/vorix/gatewayservice/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.vorix.gatewayservice.filter;
 
+import com.vorix.gatewayservice.constants.HeaderConstants;
 import com.vorix.gatewayservice.security.jwt.JwtService;
 import com.vorix.gatewayservice.security.jwt.JwtTokenType;
 import com.vorix.gatewayservice.util.PublicEndpointValidator;
@@ -9,9 +10,13 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -61,7 +66,29 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
             return exchange.getResponse().setComplete();
         }
 
-        return chain.filter(exchange);
+        UUID userId = jwtService.extractUserId(token);
+
+        String email = jwtService.extractEmail(token);
+
+        Set<String> roles = jwtService.extractRoles(token);
+
+        System.out.println("Authenticated User:");
+        System.out.println("User ID: " + userId);
+        System.out.println("Email: " + email);
+        System.out.println("Roles: " + roles);
+
+        ServerHttpRequest mutatedRequest = exchange.getRequest()
+                .mutate()
+                .header(HeaderConstants.USER_ID, userId.toString())
+                .header(HeaderConstants.USER_EMAIL, email)
+                .header(HeaderConstants.USER_ROLES, String.join(",", roles))
+                .build();
+
+        ServerWebExchange mutatedExchange = exchange.mutate()
+                                                    .request(mutatedRequest)
+                                                    .build();
+
+        return chain.filter(mutatedExchange);
     }
 
     @Override
